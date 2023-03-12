a5d300b012061ba35e10e237594031f1a3ae7e67
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-03-12 23:54:44 +0300

    Set tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      yellow: "#FDC913",
+      "light-gray": "#5F5F5F",
+      "dark-gray": "#292929",
+      red: "#CE2829",
+      beige: "#FAF7F2",
+    },
+    extend: {
+      fontFamily: {
+        Barlow: ["Barlow"],
+        Quattrocento: ["Quattrocento"],
+        Satisfy: ["Satisfy"],
+      },
+      backgroundImage: {
+        home: "url('./assets/mvp-banner.png')",
+      },
+    },
+  },
+  plugins: [],
+};
