0bc12dbbc9d54b710571e9383dd38db2a048d3e8
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-03-13 22:33:55 +0300

    Add data

---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+const Data = [
+  { id: "1", value: "Pepperroni", status: true },
+  { id: "2", value: "Domates", status: false },
+  { id: "3", value: "Biber", status: false },
+  { id: "4", value: "Sosis", status: true },
+  { id: "5", value: "Mısır", status: true },
+  { id: "6", value: "Sucuk", status: false },
+  { id: "7", value: "Kanada Jambonu", status: false },
+  { id: "8", value: "Pastırma", status: false },
+  { id: "9", value: "Ananas", status: true },
+  { id: "10", value: "Tavuk Izgara", status: false },
+  { id: "11", value: "Jalepeno", status: true },
+  { id: "12", value: "Kabak", status: true },
+  { id: "13", value: "Soğan", status: false },
+  { id: "14", value: "Sarımsak", status: false },
+];
+
+export default Data;
