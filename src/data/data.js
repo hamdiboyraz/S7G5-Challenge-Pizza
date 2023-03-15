5e47134160228fe2cb4a6340fbe18a4239e0cba9
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-03-16 00:24:07 +0300

    Update data

--- src/data/data.js
+++ src/data/data.js
@@ -1,18 +1,16 @@
-const Data = [
-  { id: "1", value: "Pepperroni", status: true },
-  { id: "2", value: "Domates", status: false },
-  { id: "3", value: "Biber", status: false },
-  { id: "4", value: "Sosis", status: true },
-  { id: "5", value: "Mısır", status: true },
-  { id: "6", value: "Sucuk", status: false },
-  { id: "7", value: "Kanada Jambonu", status: false },
-  { id: "8", value: "Pastırma", status: false },
-  { id: "9", value: "Ananas", status: true },
-  { id: "10", value: "Tavuk Izgara", status: false },
-  { id: "11", value: "Jalepeno", status: true },
-  { id: "12", value: "Kabak", status: true },
-  { id: "13", value: "Soğan", status: false },
-  { id: "14", value: "Sarımsak", status: false },
+export const toppings = [
+  { id: "1", name: "Pepperroni", price: 5, isChecked: false },
+  { id: "2", name: "Domates", price: 5, isChecked: false },
+  { id: "3", name: "Biber", price: 5, isChecked: false },
+  { id: "4", name: "Sosis", price: 5, isChecked: false },
+  { id: "5", name: "Mısır", price: 5, isChecked: false },
+  { id: "6", name: "Sucuk", price: 5, isChecked: false },
+  { id: "7", name: "Kanada Jambonu", price: 5, isChecked: false },
+  { id: "8", name: "Pastırma", price: 5, isChecked: false },
+  { id: "9", name: "Ananas", price: 5, isChecked: false },
+  { id: "10", name: "Tavuk Izgara", price: 5, isChecked: false },
+  { id: "11", name: "Jalepeno", price: 5, isChecked: false },
+  { id: "12", name: "Kabak", price: 5, isChecked: false },
+  { id: "13", name: "Soğan", price: 5, isChecked: false },
+  { id: "14", name: "Sarımsak", price: 5, isChecked: false },
 ];
-
-export default Data;
